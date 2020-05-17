5ed4b795982e0e6979c9d1715e5a1bd39f0e5b3f
NOOK <n.kittipoj@gmail.com>
2020-05-17 09:58:48 +0700

    initial microservice

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MqttModule } from './mqtt.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MqttModule,
+    {
+      transport: Transport.MQTT,
+      options: {
+        url: `mqtt:Josshome.thddns.net`,
+        port: 3225
+      }
+    },
+  );
+  microservice.listen(() => {
+    console.log('Microservice is listening')
+  });
 }
 bootstrap();
